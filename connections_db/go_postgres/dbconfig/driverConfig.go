b9cfa62bbc3d638c58903790d89c850ca770be16
SylarK <syst.lucasamado@gmail.com>
2021-04-17 11:39:14 -0300

    Studying connections in the database. postgresql and golang

---
+++ connections_db/go_postgres/dbconfig/driverConfig.go
@@ -0,0 +1,28 @@
+// Arquivo sera usado como pacote e importado para o main
+
+package dbconfig
+
+import "fmt"
+
+// Definindo tabela criada no banco para teste
+type Article struct {
+	ID    int
+	Title string
+	Body  []byte
+}
+
+const PostgresDriver = "postgres"
+
+const User = "postgres"
+
+const Host = "localhost"
+
+const Port = "5432"
+
+const Password = "masterkey"
+
+const DbName = "Go"
+
+const TableName = "article"
+
+var DataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
---
+++ connections_db/go_postgres/go.mod
@@ -0,0 +1,7 @@
+module go_pg
+
+go 1.16
+
+require github.com/lib/pq v1.10.0 // indirect
+
+//replace github.com/lib/pq v1.10.0 => {E:/golang/connections_db/go_postgres}
